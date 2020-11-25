ddacd3f880e21fe005d659b767a327e51d2e8c06
Nathaniel Salazar <nlpsalazar@gmail.com>
2020-11-24 22:36:13 -0500

    EOSIO Tests

---
+++ tests/delete_small.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cleos push action hospital.vt delpatient '[hospital.vt,donnicatteri]' -p hospital.vt 
+cleos push action hospital.vt delpatient '[hospital.vt,letitiaserra]' -p hospital.vt 
+cleos push action hospital.vt delpatient '[hospital.vt,watbaldi]' -p hospital.vt 
+cleos push action hospital.vt delpatient '[hospital.vt,kellyloins]' -p hospital.vt 
+cleos push action hospital.vt delpatient '[hospital.vt,algernonbran]' -p hospital.vt 
+cleos push action hospital.vt delpatient '[hospital.vt,michelewille]' -p hospital.vt 
+cleos push action hospital.vt delpatient '[hospital.vt,violanteanto]' -p hospital.vt 
+cleos push action hospital.vt delpatient '[hospital.vt,iveodonohue]' -p hospital.vt 
+cleos push action hospital.vt delpatient '[hospital.vt,tremaineedri]' -p hospital.vt 
+cleos push action hospital.vt delpatient '[hospital.vt,rafaelllegru]' -p hospital.vt 
---
+++ tests/update_small.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cleos push action hospital.vt modvital '[hospital.vt,mellisentwoo,99.38708292066566, 64.30794159417613, 13.033869913512229, 86.40038045813482]' -p hospital.vt 
+cleos push action hospital.vt modvital '[hospital.vt,pavlaspyking,97.84429410713885, 74.84558979720772, 14.397362755659254, 100.26583177815438]' -p hospital.vt 
+cleos push action hospital.vt modvital '[hospital.vt,aubreychuck,98.9910008709298, 69.58513290835542, 16.938623907530705, 139.58314019534313]' -p hospital.vt 
+cleos push action hospital.vt modvital '[hospital.vt,teriannvila,100.91943872509108, 88.40486831113924, 13.318330914517446, 135.81806402226908]' -p hospital.vt 
+cleos push action hospital.vt modvital '[hospital.vt,sharaislewco,100.95427543552476, 88.11679804280408, 14.36770008481853, 100.69421619030142]' -p hospital.vt 
+cleos push action hospital.vt modvital '[hospital.vt,jackiechella,98.45896121813823, 91.56031049813248, 14.078155644846394, 89.34416818120127]' -p hospital.vt 
+cleos push action hospital.vt modvital '[hospital.vt,rowlandmerck,97.04806604401338, 77.74943507468411, 16.82262352215433, 100.58057911267181]' -p hospital.vt 
+cleos push action hospital.vt modvital '[hospital.vt,marylouscull,98.59182595913714, 76.6077941732361, 11.823857069862964, 123.19740492694399]' -p hospital.vt 
+cleos push action hospital.vt modvital '[hospital.vt,tedblaydon,100.21812856319681, 61.38695030156135, 10.119336294381712, 137.19515747724893]' -p hospital.vt 
+cleos push action hospital.vt modvital '[hospital.vt,claretabrida,97.80982702523792, 96.74260386124217, 15.403747404004474, 118.65288263700839]' -p hospital.vt 
